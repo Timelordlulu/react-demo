ba066c032f7df89e05ca86dddf025f8bc188dea8
Lu, Eric <timelordlu@gmail.com>
2021-04-04 15:49:20 -0500

    add a datepicker input form

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+export default function MyComponent() {
+  const [date, setDate] = useState(new Date());
+  const handleChange = date => setDate(date);
+
+  return <DatePicker selected={date} onChange={handleChange} />;
 }
-
-export default App;
