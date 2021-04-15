f8dfdc813d4d9974bc3fc17616f28a542bd9b31f
Lu, Eric <timelordlu@gmail.com>
2021-04-15 17:30:47 -0500

    add pop up window for submit, log the input when submit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -6,6 +6,9 @@ import "react-datepicker/dist/react-datepicker.css";
 export default function MyComponent() {
   const [date, setDate] = useState(new Date());
   const handleChange = date => setDate(date);
-
+  var submit = document.getElementById("submit");
+  submit.onclick = function() {
+    console.log(date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear());
+  }
   return <DatePicker selected={date} onChange={handleChange} />;
 }
