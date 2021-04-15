bc00143669542df8536e014fc1cf8eb446189c41
Lu, Eric <timelordlu@gmail.com>
2021-04-15 18:45:00 -0500

    use react-table to add a plain table

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import MyComponent from './App';
+import App from './Table';
 
 ReactDOM.render(
   <React.StrictMode>
     <MyComponent />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('date-picker')
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+   <App />
+  </React.StrictMode>,
+  document.getElementById('table')
 );
 
 // If you want to start measuring performance in your app, pass a function
